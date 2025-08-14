e3055463628b9190b4804a386c3bc94f695455bc
Andres Callejas <andres.callejas@iudigital.edu.co>
2025-08-14 02:50:48 +0000

    clase 1 : 2025/08/13 - 9:50pm

---
+++ prueba.py
@@ -0,0 +1,9 @@
+def area_rectangulo(base :float ,alto :float) -> float:
+    """Calcular el area del triangulo
+    :param base: base del triangulo
+    :param alto: altura del triagulo
+    :return: area del triangulo
+    """
+    return base * alto
+
+print("Area de 3x5",area_rectangulo("M",5))
